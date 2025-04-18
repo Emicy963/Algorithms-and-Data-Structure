97f94a109f5522e8c6d5d077a4324ef3db687e8e
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-04-18 20:53:04 +0100

    Solving first exercice

---
+++ src/ex1.py
@@ -0,0 +1,22 @@
+def add_element_list(elem: int):
+    vector: int = list()
+    vector.append(elem)
+    return vector
+
+def show_elements(vectors: list):
+    for i, j in enumerate(vectors, start=1):
+        print(f'{i}º {j}')
+
+def main():
+    print('===Create Interger Vectors===')
+    ask = int(input("Add[1] or show[2]: "))
+    if ask==1:
+        for i in range(10):
+            try:
+                print(F'Less {10-i}') if i !=10 else print('Last one!')
+                elem: int = int(input('Write a number: '))
+                vector = add_element_list(elem)
+            except Exception as err:
+                print(f'Erro: {str(err)}')
+    if ask==2:
+        show_elements(vector)
